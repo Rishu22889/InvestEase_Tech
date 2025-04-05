565875a4b1b7409eba9cc918f740d3058c05c36a
xyrosoul <24je0616@iitism.ac.in>
2025-04-05 09:01:58 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  FacebookAuthProvider
+} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABqgualjotxDkYaQ9uMtbRV-mQveGdL5s",
+    authDomain: "invest-ease-8d7f6.firebaseapp.com",
+    projectId: "invest-ease-8d7f6",
+    storageBucket: "invest-ease-8d7f6.firebasestorage.app",
+    messagingSenderId: "757708392550",
+    appId: "1:757708392550:web:2d2f4058e6b58fcb2445a7"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+export { auth, googleProvider, facebookProvider };
